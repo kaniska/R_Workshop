5ebd6f1f0f404fda7d455461631f301cb9cfd3f0
kaniska <kaniska.mandal@gmail.com>
2015-08-15 10:50:58 -0700

    Update weatherDataAnalysis.R

--- weatherDataAnalysis.R
+++ weatherDataAnalysis.R
@@ -7,11 +7,17 @@ dim(x)
 print(x[152:153,])
 print(x$Ozone[47])
 summary(x$Ozone)
+# What is the mean of the Ozone column in this dataset? Exclude missing values (coded as NA) from this calculation.
 mean(na.exclude(x$Ozone))
+# How many missing values are in the Ozone column of this data frame?
+
 y <- x[x$Ozone > 31 & x$Temp > 90, ]
 mean(na.exclude(y$Solar.R))
+# Extract the subset of rows of the data frame where Ozone values are above 31 and Temp values are above 90. What is the mean of Solar.R in this subset?
 mean(na.exclude(x$Solar.R[x$Ozone > 31 & x$Temp > 90]))
+# What is the mean of "Temp" when "Month" is equal to 6?
 mean(na.exclude(x$Temp[x$Month == 6]))
+# What was the maximum ozone value in the month of May (i.e. Month = 5)?
 max(na.exclude(x$Ozone[x$Month == 5]))
 x <- 1:4
 y <- 2:3
