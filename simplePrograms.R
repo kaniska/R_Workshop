6a8b307665fc46b465bce5c343799d5bfd9c97d7
kaniska <kaniska.mandal@gmail.com>
2015-08-15 11:07:46 -0700

    Create simplePrograms.R

---
+++ simplePrograms.R
@@ -0,0 +1,36 @@
+# Loop Char sequence
+x <- c(“a” , “b” , “c” , “d” )
+for(i in seq_along(x)) {
+    print(x)
+}
+# Loop 2
+for(i in 1:4) {
+  print(x[i])
+}
+# Loop Matrix
+x <- matrix(1:6, 2 , 3)
+for(i in seq_len(nrow(x))) {
+     for(j in seq_len(ncol(x))) {
+            print(x[i,j])
+     }
+}
+# Loop random binomials
+while(z >= 3 && z <= 10) {
+  coin <- rbinom(1,1, 0.5)
+  if (coin == 1) {
+     z <- z + 1
+  } else {
+      z <- z - 1
+  }
+}
+# Loop 4
+x0 <- 1
+t01  <-  le - 8
+repeat {
+   x1 <- computeEstimate()
+   if(abs(x1 - x0) < t01)  {
+       break
+   } else {
+       x0 <-  x1
+   }
+}
