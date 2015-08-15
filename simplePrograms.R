9bf95d2e69442a3ca2d7c9c820e27ee78a5fee67
kaniska <kaniska.mandal@gmail.com>
2015-08-15 11:16:41 -0700

    Update simplePrograms.R

--- simplePrograms.R
+++ simplePrograms.R
@@ -34,3 +34,13 @@ repeat {
        x0 <-  x1
    }
 }
+#
+columnmean <- function(y) {
+  nc <- ncol(y)
+  means <- numeric(nc)
+  for(i in 1:nc) {
+    means[i] <- mean(y[,i], na.rm = removeNA)
+  }
+  means
+}
+#
